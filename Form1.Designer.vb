25e83a1aef2b2b6d8bbe7ee7e283368728faf215
Naara Taborda <naara.taborda@gmail.com>
2026-04-10 17:55:31 -0600

    Diseño inicial de Form1 con controles y recursos añadidos
    Se genera el código de diseño para Form1, incluyendo un DataGridView, 9 botones y 3 GroupBox, con su configuración y disposición. Se añade el archivo de recursos Form1.resx y se referencia en el proyecto para gestionar recursos del formulario. Se ajusta el tamaño del formulario para acomodar todos los controles.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,252 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 14)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1316, 755)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1095, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(189, 54)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Insertar&"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1095, 84)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(189, 54)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Actualizar&"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(1095, 159)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(189, 54)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Borrar&"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(1114, 293)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(189, 54)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Insertar&"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(1114, 366)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(189, 54)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "Actualizar&"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(1114, 437)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(189, 54)
+        Me.Button6.TabIndex = 8
+        Me.Button6.Text = "Borrar&"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(1114, 538)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(189, 54)
+        Me.Button7.TabIndex = 9
+        Me.Button7.Text = "Insertar&"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(1114, 605)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(189, 54)
+        Me.Button8.TabIndex = 10
+        Me.Button8.Text = "Actualizar&"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(1114, 685)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(189, 54)
+        Me.Button9.TabIndex = 11
+        Me.Button9.Text = "Borrar&"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(19, 23)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1295, 231)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button10)
+        Me.GroupBox2.Controls.Add(Me.Button11)
+        Me.GroupBox2.Controls.Add(Me.Button12)
+        Me.GroupBox2.Location = New System.Drawing.Point(23, 275)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(1295, 231)
+        Me.GroupBox2.TabIndex = 13
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(1095, 159)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(189, 54)
+        Me.Button10.TabIndex = 5
+        Me.Button10.Text = "Borrar&"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(1095, 84)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(189, 54)
+        Me.Button11.TabIndex = 4
+        Me.Button11.Text = "Actualizar&"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(1095, 10)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(189, 54)
+        Me.Button12.TabIndex = 3
+        Me.Button12.Text = "Insertar&"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button13)
+        Me.GroupBox3.Controls.Add(Me.Button14)
+        Me.GroupBox3.Controls.Add(Me.Button15)
+        Me.GroupBox3.Location = New System.Drawing.Point(23, 521)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(1295, 231)
+        Me.GroupBox3.TabIndex = 14
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "GroupBox3"
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(1095, 159)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(189, 54)
+        Me.Button13.TabIndex = 5
+        Me.Button13.Text = "Borrar&"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(1095, 84)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(189, 54)
+        Me.Button14.TabIndex = 4
+        Me.Button14.Text = "Actualizar&"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(1095, 10)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(189, 54)
+        Me.Button15.TabIndex = 3
+        Me.Button15.Text = "Insertar&"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1340, 781)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
 End Class
